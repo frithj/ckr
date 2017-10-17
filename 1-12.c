fbd1d613660f6a5539204d394885a952947ea006
frithj <32688509+frithj@users.noreply.github.com>
2017-10-17 19:45:32 +0200

    exercise 1-12 on page 21

---
+++ 1-12.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define IN  1   /* inside word */
+#define OUT 0   /* outside word */
+
+/* print input one word per line */
+int main()
+{ 
+    int c, state;
+
+    state = OUT;
+    while ((c = getchar()) != EOF) {
+        if (c != '\n' && c != '\t' && c != ' ') {
+            state = IN;
+            putchar(c);
+        } else if (state == IN) {
+            state = OUT;
+            putchar('\n');
+        }
+    }
+}
